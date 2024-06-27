c1f745a83357b1669cb9c24bc93343cd8f4a8054
Suraj Thapa <surajthapart@gmail.com>
2024-06-27 16:07:09 +0545

    Setting up a container for application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
 
   return (
-    <>
-      <p className="text-3xl font-bold">App</p>
+    <div className="h-[90vh] w-[90vw] bg-blue-200/50 rounded-md backdrop-blur-sm backdrop-saturate-150">
       
-    </>
+    </div>
   )
 }
 
