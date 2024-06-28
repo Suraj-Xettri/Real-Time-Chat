b3c03f867865f705cd64ffc98baae8265604c03b
Suraj Thapa <surajthapart@gmail.com>
2024-06-28 14:34:40 +0545

    chat section ui completed

--- src/Components/chats/Chat.jsx
+++ src/Components/chats/Chat.jsx
@@ -9,31 +9,26 @@ const Chat = () => {
 
         <div className='chat flex-1 p-3 flex flex-col gap-5 overflow-scroll'>
           <div className="message flex gap-2">
-            <FaCircleUser/>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt a eligendi autem ipsum, obcaecati, aspernatur optio maiores molestias mollitia totam eum blanditiis asperiores expedita at facere, odit iure ipsa fugit.</p>
-          </div>
-
-          <div className="message flex gap-2">
-            <FaCircleUser/>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt a eligendi autem ipsum, obcaecati, aspernatur optio maiores molestias mollitia totam eum blanditiis asperiores expedita at facere, odit iure ipsa fugit.</p>
-          </div>
+          <img src="/user.png" alt="" className='w-7 h-7 rounded-full' />
+            <p>Hi</p>
+            </div>
 
           <div className="message owner flex gap-2">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt a eligendi autem ipsum, obcaecati, aspernatur optio maiores molestias mollitia totam eum blanditiis asperiores expedita at facere, odit iure ipsa fugit.</p>
+            <p>hello</p>
           </div>
 
           <div className="message flex gap-2 items-center">
-            <FaCircleUser className='text-[100px]'/>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt a eligendi autem ipsum, obcaecati, aspernatur optio maiores molestias mollitia totam eum blanditiis asperiores expedita at facere, odit iure ipsa fugit.</p>
-          </div>
+            <img src="/user.png" alt="" className='w-7 h-7 rounded-full' />
+            <p>What are you doing</p>      
+    </div>
 
           <div className="message owner flex gap-2">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt a eligendi autem ipsum, obcaecati, aspernatur optio maiores molestias mollitia totam eum blanditiis asperiores expedita at facere, odit iure ipsa fugit.</p>
+            <p>fine and you</p>
           </div>
 
           <div className="message flex gap-2">
-            <FaCircleUser/>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt a eligendi autem ipsum, obcaecati, aspernatur optio maiores molestias mollitia totam eum blanditiis asperiores expedita at facere, odit iure ipsa fugit.</p>
+            <img src="/user.png" alt="" className='w-7 h-7 rounded-full' />
+            <p>Same here dude</p>            
           </div>
         </div>
         <ChatButton/>
