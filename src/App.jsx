a2f289baf9e28c9b1a9ce304b97f39e86f7b8a15
Suraj Thapa <surajthapart@gmail.com>
2024-06-30 16:15:32 +0545

    Creating log in flow and puttion condition for user log in

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Chat from "./Components/chats/Chat"
 import Detail from "./Components/detail/Detail"
 import Friends from "./Components/friends/Friends"
+import Login from "./Components/login/Login"
 function App() {
+  const user = false
   return (
     <div className="h-[90vh] w-[90vw] flex bg-blue-200/50 rounded-md backdrop-blur-sm backdrop-saturate-150">
+      {user ? (
+      <>
         <Friends/>
-        {/* <Chat/> */}
-        <Detail/>
+        <Chat/>
+        <Detail/> 
+      </> )
+        : (
+          <Login/>
+        )
+        }
     </div>
   )
 }
