d1190f83b54f5ff856444a88515635d6ceedeaea
Suraj Thapa <surajthapart@gmail.com>
2024-06-30 17:54:41 +0545

    Made a sign up page with image selection

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [avtar, setAvtar] = useState({
+    file: null,
+    url: ''
+  });
 
-export default Login
+  const handleAvtar = (e) => {
+    if (e.target.files[0]) {
+      setAvtar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0])
+      });
+    }
+  };
+
+  return (
+    <div className='flex w-full h-full gap-24 justify-between items-center p-5'>
+      <div className='flex-1 flex flex-col gap-5 items-center'>
+        <p>Welcome To React Chat</p>
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Log In
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="seperate"></div>
+
+      <div className='flex-1 flex flex-col gap-5 items-center'>
+        <h2>Create an Account</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              id="username"
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your username"
+              name='username'
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your email"
+              name='email'
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your password"
+              name='password'
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="image">
+              Upload Image
+            </label>
+            <img src={avtar.url || "/zoro.jpg"} alt="Url Avtar" className='w-10 h-10 rounded-full' />
+            <input
+              id="image"
+              type="file"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleAvtar}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
