8e608b5783aee17b6a649158c965c72013012e80
Suraj Thapa <surajthapart@gmail.com>
2024-07-06 13:48:50 +0545

    ouffffffff i hateeeeeeee codinggggggggggggggggggggggggggggggggggg

--- src/Components/chats/Chat.jsx
+++ src/Components/chats/Chat.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react'
-import ChatHead from './ChatHead'
-import ChatButton from './ChatButton'
-import { arrayUnion, doc, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useRef, useState } from 'react';
+import ChatHead from './ChatHead';
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from '../../library/Firebase';
 import { userChatStore } from '../../library/chatStore';
 import { FaRegImage } from "react-icons/fa";
@@ -9,90 +8,103 @@ import { IoCameraSharp } from "react-icons/io5";
 import { MdKeyboardVoice } from "react-icons/md";
 import { FaRegFaceSmile } from "react-icons/fa6";
 import { userStore } from '../../library/userStore';
+
 const Chat = () => {
+  const [chat, setChat] = useState();
+  const endRef = useRef(null);
+  const [message, setMessage] = useState("");
 
-  const [chat, setChat] = useState()
-  const endRef = useRef(null)
-  const [message, setMessage] = useState("")
-
-  const {currentUser} = userStore()
-
-  const {chatId} = userChatStore()
+  const { currentUser } = userStore();
+  const { chatId, user } = userChatStore();
 
   useEffect(() => {
-    endRef.current?.scrollIntoView({behavior: "smooth"})
-  },[])
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chat]);
 
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", chatId), (res) => {
-        setChat(res.data())
-    })
+      setChat(res.data());
+    });
     return () => {
-      unSub()
-    }
-  },[chatId])
-
-
+      unSub();
+    };
+  }, [chatId]);
 
   const handleMessage = (e) => {
-    setMessage(e.target.value)
-  }
+    setMessage(e.target.value);
+  };
 
   const handleSend = async () => {
-    if(message == "")return
+    if (message === "") return;
 
     try {
-      await updateDoc(doc(db,"chats", chatId),{
-        messages:arrayUnion({
+      await updateDoc(doc(db, "chats", chatId), {
+        messages: arrayUnion({
           senderId: currentUser.id,
-          text,
+          message,
           createdAt: new Date()
-
         })
-      })
-    } catch (error) {
-      console.log(error)
-    }
+      });
 
-  }
+      const userIds = [currentUser.id, user.id];
+
+      userIds.forEach(async (id) => {
+        const userChatRef = doc(db, "usersChats", id);
+        const userChatSnapshot = await getDoc(userChatRef);
+
+        if (userChatSnapshot.exists()) {
+          const userChatsData = userChatSnapshot.data();
+
+          const chatIndex = userChatsData.chats.findIndex(
+            c => c.chatId === chatId
+          );
+
+          userChatsData.chats[chatIndex].lastMessage = true;
+          userChatsData.chats[chatIndex].isSeen =
+            id === currentUser.id ? true : false;
+          userChatsData.chats[chatIndex].updatedAt = Date.now();
+
+          await updateDoc(userChatRef, {
+            chats: userChatsData.chats,
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className='flex-[2] border-r flex flex-col'>
-        <ChatHead/>
+      <ChatHead />
 
-        <div className='chat flex-1 p-3 flex flex-col gap-5 overflow-scroll'>
-
-          {chat?.messages?.map((messages)=>{
-            <div className="message flex gap-2" key={messages?.createAt}>
-              <img src="/user.png" alt="" className='w-7 h-7 rounded-full' />
-              <p>{messages.text}</p>
-            </div>
-          })}
-          
-
-          <div className="message owner flex gap-2">
-            <p>fine and you</p>
+      <div className='chat flex-1 p-3 flex flex-col gap-5 overflow-scroll'>
+        {chat?.messages?.map((message) => (
+          <div className="message flex gap-2" key={message.createdAt}>
+            <img src="/user.png" alt="" className='w-7 h-7 rounded-full' />
+            <p>{message.message}</p>
           </div>
+        ))}
 
-          <div ref={endRef}></div>
+        <div ref={endRef}></div>
+      </div>
+
+      <div className='flex border-t justify-between p-3 items-center gap-3'>
+        <div className='flex gap-3 text-2xl'>
+          <FaRegImage className='cursor-pointer' />
+          <IoCameraSharp className='cursor-pointer' />
+          <MdKeyboardVoice className='cursor-pointer' />
         </div>
-        
-        <div className='flex border-t justify-between p-3 items-center gap-3'>
-          <div className='flex gap-3 text-2xl'>
-              <FaRegImage className='cursor-pointer'/>
-              <IoCameraSharp className='cursor-pointer'/>
-              <MdKeyboardVoice className='cursor-pointer'/>
-          </div>
-          <input type="text" onChange={handleMessage} placeholder='Type a amessage ' className='p-2 flex-1 border-none rounded-xl bg-gray-700 outline-none text-white'/>
+        <input type="text" onChange={handleMessage} placeholder='Type a message' className='p-2 flex-1 border-none rounded-xl bg-gray-700 outline-none text-white' />
 
-          <div>
-              <FaRegFaceSmile className='text-2xl cursor-pointer'/>
-          </div>
-
-          <button>Send</button>
+        <div>
+          <FaRegFaceSmile className='text-2xl cursor-pointer' />
         </div>
+
+        <button onClick={handleSend}>Send</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
